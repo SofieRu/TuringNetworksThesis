2ba63c3c2b020fb908b63756aa610d1d90a22d72
Sofie R <sofie@rueffer.com>
2026-03-26 14:48:21 +0000

    new changes

---
+++ TopologyRanking/Topology3954/3954-lhs-3node-hpc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# LHS Analysis for Topology #3954
+
+import numpy as np
+import matplotlib.pyplot as pl
+from numpy.linalg import eigvals
+from scipy.optimize import fsolve
+from scipy.stats import qmc
+
+
+print("hello world")
