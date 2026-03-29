0da7e5b9547b92bcc857c0ae1cf16a99865e6834
Sofie <sofie@rueffer.com>
2026-03-30 00:14:16 +0100

    Add extended diffusion configurations

--- TopologyRanking/Topology3954/3954-lhs-3node-hpc.py
+++ TopologyRanking/Topology3954/3954-lhs-3node-hpc.py
@@ -165,6 +165,7 @@ def is_turing_shaberi(J, eigs_0, DU, DV, DW):
 #     14: {"name": "DCI_Type3", "dU": 0.0,  "dV": 1.0,  "dW": 0.0},
 # }
 
+# EXTENDED VERSION
 
 DIFFUSION_CONFIGS = {
     # DCC: A=Destable, B=Complementary, C=Complementary
@@ -182,45 +183,50 @@ DIFFUSION_CONFIGS = {
     9:  {"name": "DCC_Type3_Unequal2", "dU": 0.0,  "dV": 1.0,  "dW": 10.0},
     
     # CDD: A=Complementary, B=Destable, C=Destable
-    6:  {"name": "CDD_Type1",          "dU": 10.0, "dV": 1.0,  "dW": 1.0},
-    7:  {"name": "CDD_Type1_Control",  "dU": 1.0,  "dV": 1.0,  "dW": 1.0},   
+    10: {"name": "CDD_Type1",          "dU": 10.0, "dV": 1.0,  "dW": 1.0},
+    11: {"name": "CDD_Type1_Control",  "dU": 1.0,  "dV": 1.0,  "dW": 1.0},   
     
-    8:  {"name": "CDD_Type2_Equal",    "dU": 0.0,  "dV": 1.0,  "dW": 1.0},
-    9:  {"name": "CDD_Type2_Unequal1", "dU": 0.0,  "dV": 0.1,  "dW": 1.0},
-    9:  {"name": "CDD_Type2_Unequal2", "dU": 0.0,  "dV": 1.0,  "dW": 10.0},
+    12: {"name": "CDD_Type2_Equal",    "dU": 0.0,  "dV": 1.0,  "dW": 1.0},
+    13: {"name": "CDD_Type2_Unequal1", "dU": 0.0,  "dV": 0.1,  "dW": 1.0},
+    14: {"name": "CDD_Type2_Unequal2", "dU": 0.0,  "dV": 1.0,  "dW": 10.0},
     
-    10: {"name": "CDD_Type3_Equal",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},
-    11: {"name": "CDD_Type3_Unequal1", "dU": 1.0,  "dV": 0.1,  "dW": 0.0},
-    12: {"name": "CDD_Type3_Unequal2", "dU": 10.0, "dV": 1.0,  "dW": 0.0},
-    13: {"name": "CDD_Type3_Var",      "dU": 1.0,  "dV": 0.0,  "dW": 1.0},
+    15: {"name": "CDD_Type3_Equal",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},
+    16: {"name": "CDD_Type3_Unequal1", "dU": 1.0,  "dV": 0.1,  "dW": 0.0},
+    17: {"name": "CDD_Type3_Unequal2", "dU": 10.0, "dV": 1.0,  "dW": 0.0},
+    18: {"name": "CDD_Type3_Var",      "dU": 1.0,  "dV": 0.0,  "dW": 1.0},
     
     # CCD: A=Compl., B=Compl., C=Destable
-    12: {"name": "CCD_Type1",          "dU": 10.0, "dV": 10.0, "dW": 1.0},
-    13: {"name": "CCD_Type1_Control",    "dU": 1.0,  "dV": 1.0,  "dW": 1.0},   # Control: should fail
+    19: {"name": "CCD_Type1",          "dU": 10.0, "dV": 10.0, "dW": 1.0},
+    20: {"name": "CCD_Type1_OneFast",  "dU": 10.0, "dV": 1.0,  "dW": 1.0},
+    21: {"name": "CCD_Type1_Control",  "dU": 1.0,  "dV": 1.0,  "dW": 1.0},
     
-    14: {"name": "CCD_Type2",          "dU": 0.0,  "dV": 1.0,  "dW": 1.0},
+    22: {"name": "CCD_Type2_Equal",    "dU": 0.0,  "dV": 1.0,  "dW": 1.0},
+    23: {"name": "CCD_Type2_Unequal1", "dU": 0.0,  "dV": 0.1,  "dW": 1.0},
+    24: {"name": "CCD_Type2_Unequal2", "dU": 0.0,  "dV": 1.0,  "dW": 10.0},
+    25: {"name": "CCD_Type2_Var",      "dU": 1.0,  "dV": 0.0,  "dW": 1.0},
     
-    15: {"name": "CCD_Type3_Equal",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},   # Current
-    16: {"name": "CCD_Type3_Unequal1", "dU": 0.1,  "dV": 1.0,  "dW": 0.0},   # Lower A diffusion
-    17: {"name": "CCD_Type3_Unequal2", "dU": 10.0, "dV": 1.0,  "dW": 0.0},   # Higher A diffusion
+    26: {"name": "CCD_Type3_Equal",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},
+    27: {"name": "CCD_Type3_Unequal1", "dU": 0.1,  "dV": 1.0,  "dW": 0.0},
+    28: {"name": "CCD_Type3_Unequal2", "dU": 10.0, "dV": 1.0,  "dW": 0.0},
     
     # DCD: A=Destable, B=Compl., C=Destable
-    18: {"name": "DCD_Type1",          "dU": 1.0,  "dV": 10.0, "dW": 1.0},
-    19: {"name": "DCD_Type1_Control",    "dU": 1.0,  "dV": 1.0,  "dW": 1.0},   # Control: should fail
+    29: {"name": "DCD_Type1",          "dU": 1.0,  "dV": 10.0, "dW": 1.0},
+    30: {"name": "DCD_Type1_Control",  "dU": 1.0,  "dV": 1.0,  "dW": 1.0},
     
-    20: {"name": "DCD_Type2",          "dU": 1.0,  "dV": 0.0,  "dW": 1.0},
+    31: {"name": "DCD_Type2_Equal",    "dU": 1.0,  "dV": 0.0,  "dW": 1.0},
+    32: {"name": "DCD_Type2_Unequal1", "dU": 0.1,  "dV": 0.0,  "dW": 1.0},
+    33: {"name": "DCD_Type2_Unequal2", "dU": 1.0,  "dV": 0.0,  "dW": 10.0},
     
-    21: {"name": "DCD_Type3_Equal",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},   # Current
-    22: {"name": "DCD_Type3_Unequal1", "dU": 0.1,  "dV": 1.0,  "dW": 0.0},   # Lower A diffusion
-    23: {"name": "DCD_Type3_Unequal2", "dU": 10.0, "dV": 1.0,  "dW": 0.0},   # Higher A diffusion
+    34: {"name": "DCD_Type3_Equal",    "dU": 0.0,  "dV": 1.0,  "dW": 1.0},   
+    35: {"name": "DCD_Type3_Unequal1", "dU": 0.0,  "dV": 0.1,  "dW": 1.0},
+    36: {"name": "DCD_Type3_Unequal2", "dU": 0.0,  "dV": 1.0,  "dW": 10.0},
+    37: {"name": "DCD_Type3_Var",      "dU": 1.0,  "dV": 1.0,  "dW": 0.0},
     
     # DCI: A=Destable, B=Compl., C=Immobile
-    24: {"name": "DCI_Type1",          "dU": 1.0,  "dV": 10.0, "dW": 0.0},
-    25: {"name": "DCI_Type1_Control",    "dU": 1.0,  "dV": 1.0,  "dW": 0.0},   # Control: should fail
-    
-    26: {"name": "DCI_Type2",          "dU": 1.0,  "dV": 0.0,  "dW": 0.0},
-    
-    27: {"name": "DCI_Type3",          "dU": 0.0,  "dV": 1.0,  "dW": 0.0},   # Current (only B diffuses)
+    38: {"name": "DCI_Type1",          "dU": 1.0,  "dV": 10.0, "dW": 0.0},
+    39: {"name": "DCI_Type1_Control",  "dU": 1.0,  "dV": 1.0,  "dW": 0.0},
+    40: {"name": "DCI_Type2",          "dU": 1.0,  "dV": 0.0,  "dW": 0.0},
+    41: {"name": "DCI_Type3",          "dU": 0.0,  "dV": 1.0,  "dW": 0.0},
 }
 
 
