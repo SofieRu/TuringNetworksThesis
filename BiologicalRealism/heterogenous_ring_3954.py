f73c5e8cd1cdd3e082a32e4a273c41267d4d8c95
Sofie R <sofie@rueffer.com>
2026-06-09 22:16:36 +0100

    resultssss

--- BiologicalRealism/heterogenous_ring_3954.py
+++ BiologicalRealism/heterogenous_ring_3954.py
@@ -147,11 +147,11 @@ def is_turing_shaberi(J, eigs_0, DU, DV, DW):
 
 
 # CHANGE THIS TO TEST DIFFERENT CONFIGS:
-CONFIG_TO_TEST = 2 #13 is the highest for 3954 and 10 is a lot lower with 0.0359 max Type I --> COMPARE 13 and 2 or 4 !!! for thesis i think?
-CONFIG_LABEL = "low"  # or "low" — change this once when you switch configs
+CONFIG_TO_TEST = 50 #13 is the highest for 3954 and 10 is a lot lower with 0.0359 max Type I --> COMPARE 13 and 2 or 4 !!! for thesis i think?
+CONFIG_LABEL = "high"  # or "low" — change this once when you switch configs
 
 # Load parameters from CSV
-df_params = pd.read_csv('../TopologyRanking/Topology3954/3954_NEW_lhs_results_parameters.csv')
+df_params = pd.read_csv('../TopologyRanking/Topology3954/3954_PREFINAL_lhs_results_parameters.csv')
 
 # Get the best parameter set for this config
 config_data = df_params[(df_params['config_id'] == CONFIG_TO_TEST) & (df_params['param_rank'] == 1)]
