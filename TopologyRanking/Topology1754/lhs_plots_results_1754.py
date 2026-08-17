ff377d669f962cbaee6abbdb402a08aa2dc81b39
Sofie R <sofie@rueffer.com>
2026-08-17 14:52:46 +0100

    fixing the plots

--- TopologyRanking/Topology1754/lhs_plots_results_1754.py
+++ TopologyRanking/Topology1754/lhs_plots_results_1754.py
@@ -103,9 +103,9 @@ def fig_combined_overview_and_raincloud(df):
 
     #ax1.set_xticklabels(df["config_name"].str.replace(r"FINAL_LHS_3954_|Type\d*_", "", regex=True),rotation=55,ha="right",fontsize=8,)
     ax1.set_xticklabels(df["config_id"], rotation=50, ha="right", fontsize=12)
-    ax1.set_xlabel("ID of Diffusion Configurations", fontsize=15)
+    ax1.set_xlabel("ID of Diffusion Configurations", fontsize=15, color="black")
 
-    ax1.set_ylabel("Robustness Score (in %)", fontsize=15, labelpad=10)
+    ax1.set_ylabel("Robustness Score (in %)", fontsize=15, labelpad=10, color="black")
     ax1.tick_params(axis='y', labelsize=13)
     #ax1.set_title("Latin Hypercube Sampling Results, 1 million simulations\nRobustness of different diffusion rate configurations for Topology #1754",fontsize=16,loc="center",pad=10,)
     ax1.set_title("Latin Hypercube Sampling Results, $10^6$ simulations\nRobustness of different diffusion rate configurations for Topology #1754", fontsize=16, loc="center", pad=10)
@@ -150,8 +150,8 @@ def fig_combined_overview_and_raincloud(df):
             ax2.scatter(jitter,val,color=color,marker=marker,s=100,edgecolors="white",linewidths=0.4,zorder=3,)
 
     ax2.set_xticks(range(len(types)))
-    ax2.set_xticklabels(labels, fontsize=13)
-    ax2.set_ylabel("Robustness Score (in %)", fontsize=14, labelpad=10)
+    ax2.set_xticklabels(labels, fontsize=14)
+    ax2.set_ylabel("Robustness Score (in %)", fontsize=15, labelpad=10, color="black")
     ax2.tick_params(axis='y', labelsize=13)
     ax2.set_title("Robustness distribution by Type for Topology #1754",fontsize=16,loc="center",pad=10,)
     ax2.xaxis.grid(False)
@@ -166,7 +166,7 @@ def fig_combined_overview_and_raincloud(df):
 
     fig.legend(handles=bar_handles,frameon=False,loc="lower center",bbox_to_anchor=(0.3, 0.02),ncol=3,fontsize=15) # title="Turing Type"
     fig.legend(handles=rain_handles,frameon=False,loc="lower center",bbox_to_anchor=(0.7, 0.02),ncol=2,fontsize=15) # title="Diffusion Variant",
-    fig.subplots_adjust(left=0.07, right=0.95, top=0.96, bottom=0.14, hspace=0.5)
+    fig.subplots_adjust(left=0.07, right=0.95, top=0.96, bottom=0.14, hspace=0.55)
 
     save(fig, "final_1754_lhs_overview")
 
